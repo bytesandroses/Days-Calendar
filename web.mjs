d6d52743619593563e0178b6f296191b41de6d0f
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-06-16 19:40:23 +0200

    implement getCalendarRange for positions of first and last weekdays

--- web.mjs
+++ web.mjs
@@ -1,2 +1,15 @@
 import getOccurrenceDate from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
+
+function getCalendarRange(year, month) {
+  const firstDay = new Date(year, month, 1, 12, 0, 0);
+  const lastDay = new Date(year, month + 1, 0, 12, 0, 0);
+
+  const start = new Date(firstDay);
+  start.setDate(firstDay.getDate() - firstDay.getDay());
+
+  const end = new Date(lastDay);
+  end.setDate(lastDay.getDate() + (6 - lastDay.getDay()));
+
+  return { start, end };
+}
