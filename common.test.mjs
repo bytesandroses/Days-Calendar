6f680cf974ab255e59a12d3fa1265cf0f91fbce1
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-06-16 11:33:02 +0200

    integrate getOccurrenceDate() from Elisabeth branch

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,23 @@
-import { getGreeting } from "./common.mjs";
+import { getOccurrenceDate } from "./common.mjs";
 import assert from "node:assert";
 import test from "node:test";
 
-test("Greeting is correct", () => {
-  assert.equal(getGreeting(), "Hello");
+test("test for Ada Lovelace Day", () => {
+  assert.equal(getOccurrenceDate(2024, "October", "Tuesday", "second"), 8);
+});
+
+test("test for World Lemur Day", () => {
+  assert.equal(getOccurrenceDate(2024, "October", "Friday", "last"), 25);
+});
+
+test("test for International Binturong Day", () => {
+  assert.equal(getOccurrenceDate(2030, "May", "Saturday", "second"), 11);
+});
+
+test("test for International Vulture Awareness Day", () => {
+  assert.equal(getOccurrenceDate(2023, "September", "Saturday", "first"), 2);
+});
+
+test("test for International Red Panda Day", () => {
+  assert.equal(getOccurrenceDate(2025, "September", "Saturday", "third"), 20);
 });
