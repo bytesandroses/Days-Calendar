e8967cd80baba0056766a297fbc2238a46508327
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-06-16 11:06:05 +0200

    gernerateCalendar()

--- common.mjs
+++ common.mjs
@@ -1,5 +1,14 @@
 // This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
 
 export function getGreeting() {
-    return "Hello";
+  return "Hello";
+}
+
+export function generateCalendar(year, month) {
+  const calendarGrid = [];
+  const inputDate = new Date(year, month, 1);
+  const firstDay = inputDate.getDay();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  TODO;
 }
